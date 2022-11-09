abb165716a074b694862e73997f97d5cfaa60873
Jim Kocsis <jim@wecancodeit.org>
2022-11-08 22:45:13 -0500

    Separated Application into 2 Components

---
+++ src/Controllers/App.js
@@ -0,0 +1,23 @@
+import { useState, useEffect } from 'react';
+import HomeIndex from '../Views/HomeIndex';
+
+function AppGet() {
+  const requestURI = 'https://localhost:7256/api/VideoGames/';
+
+  var [videoGames, setVideoGames] = useState([]);
+
+  useEffect(() => {
+    fetch(requestURI)
+      .then((response) => response.json())
+      .then((data) => {
+        setVideoGames(data);
+      });
+  }, []);
+
+  return videoGames;
+}
+
+export default function App() {
+  var videoGames = AppGet();
+  return <HomeIndex props={videoGames} />;
+}
---
+++ src/Views/HomeIndex.js
@@ -0,0 +1,18 @@
+import { nanoid } from 'nanoid';
+
+export default function HomeIndex(props) {
+  var arr = props.props;
+  var key = "'" + nanoid() + "'";
+
+  return arr.map((element) => {
+    return (
+      <ul key={key + element.title}>
+        <li>
+          <h2>Title: {element.title}</h2>
+        </li>
+        <li>StudioId: {element.studioId}</li>
+        <li>MainCharacterId: {element.mainCharacterId}</li>
+      </ul>
+    );
+  });
+}
